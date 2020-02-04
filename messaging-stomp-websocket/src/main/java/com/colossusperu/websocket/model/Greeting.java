7182e62a7b622c6ad138e9bc9251d71decf67eb2
Juan D. Salcedo Salazar <salcedo.salazar@gmail.com>
2020-02-03 23:56:07 -0500

    [IMP] Soporte Lombok

--- messaging-stomp-websocket/src/main/java/com/colossusperu/websocket/model/Greeting.java
+++ messaging-stomp-websocket/src/main/java/com/colossusperu/websocket/model/Greeting.java
@@ -1,20 +1,12 @@
 package com.colossusperu.websocket.model;
 
+import lombok.Data;
+
+@Data
 public class Greeting {
 	private String content;
-	
-	public Greeting() {
-	}
-	
+
 	public Greeting(String content) {
-		this.content = content;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
 		this.content = content;
 	}
 }
--- messaging-stomp-websocket/src/main/java/com/colossusperu/websocket/model/Message.java
+++ messaging-stomp-websocket/src/main/java/com/colossusperu/websocket/model/Message.java
@@ -1,21 +1,8 @@
 package com.colossusperu.websocket.model;
 
+import lombok.Data;
 
+@Data
 public class Message {
 	private String name;
-	
-	public Message() {
-	}
-
-	public Message(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
